d6cbd85cb67d4f1c4c71eef91879b9cc42541892
Mike Jablonski <mjablonski@gmail.com>
2017-07-16 07:58:34 -0700

    added history list.

--- src/BrewHistory.js
+++ src/BrewHistory.js
@@ -1,14 +1,68 @@
 import React, { Component } from 'react';
-import { Jumbotron } from 'react-bootstrap';
+import { Table } from 'react-bootstrap';
+var dateFormat = require('dateformat');
 
-class BrewHistory extends Component {  
+class BrewHistory extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {brewSessions: []};
+    }
+
+    componentDidMount() {
+        fetch('http://raspberrypi.local:3001/brewSessions')
+        .then((response) => {
+            return response.json()
+        }).then((json) => {
+            this.setState({
+                brewSessions: json.data
+            }); 
+        }).catch((ex) => {
+            this.setState({
+                brewSessions: []
+            });
+        })
+    }
+
+    mapBrewSessionStatus(status) {
+        switch (status) {
+            case 1:
+                return 'Stopped';
+            case 2:
+                return 'Running';
+            case 3:
+                return 'Completed';
+            default:
+                return 'Unknown';
+        }
+    }
+
   render() {
+    const listItems = this.state.brewSessions
+        .sort((a, b) => b.created - a.created)
+        .map((item) =>
+        <tr key={item.created}>
+            <td>{item.$loki}</td>
+            <td>{item.name}</td>
+            <td>{dateFormat(item.created, "mm-dd-yyyy hh:MM:ss TT")}</td>
+            <td>{this.mapBrewSessionStatus(item.status)}</td>
+        </tr>
+        );      
     return (
         <div>
-            <Jumbotron>
-                <h1>History</h1>
-                <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-            </Jumbotron>
+            <Table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Date</th>
+                        <th>Status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listItems}
+                </tbody>
+            </Table>
         </div>
 
     );
