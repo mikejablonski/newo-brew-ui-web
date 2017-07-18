d8e421f5f45184e533ffe560b1e15c85a272b1ef
Mike Jablonski <mjablonski@gmail.com>
2017-07-18 07:33:13 -0700

    history will show status when not complete.

--- src/BrewHistory.js
+++ src/BrewHistory.js
@@ -41,6 +41,32 @@ class BrewHistory extends Component {
         this.props.history.push(`/session/${sessionId}`);
     }
 
+    resumeBrewSession = (sessionId) => () => {
+        var postBody = {};
+        postBody.sessionId = sessionId;
+
+        fetch(`http://raspberrypi.local:3001/brew/start`, 
+        {
+            method: 'POST',
+            body: JSON.stringify(postBody),
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((response) => {
+            if (!response.ok) {
+                console.log('Error!');
+            }
+            return response.json();
+        }).then((json) => {
+            // redirect to the brew page
+            this.props.history.push('/brew');
+        }).catch((ex) => {
+            console.log('Exception!');
+        });
+    }
+
   render() {
     const listItems = this.state.brewSessions
         .sort((a, b) => b.created - a.created)
@@ -49,12 +75,17 @@ class BrewHistory extends Component {
             <td>{item.$loki}</td>
             <td>{item.name}</td>
             <td>{dateFormat(item.created, "mm-dd-yyyy hh:MM:ss TT")}</td>
-            <td>{this.mapBrewSessionStatus(item.status)}</td>
+            <td>
+                {this.mapBrewSessionStatus(item.status)}
+                {item.status === 1 &&
+                    <div>Step {item.step} of 5</div>
+                }
+            </td>
             <td>
                     <ButtonToolbar>
                         <Button bsStyle="primary" onClick={this.rerunBrewSession(item.$loki)} ><Glyphicon glyph="grain" /></Button>
                         {item.status === 1 && 
-                        <Button bsStyle="success"><Glyphicon glyph="repeat" /></Button>
+                        <Button bsStyle="success" onClick={this.resumeBrewSession(item.$loki)}><Glyphicon glyph="repeat" /></Button>
                         }
                     </ButtonToolbar>
             </td>
