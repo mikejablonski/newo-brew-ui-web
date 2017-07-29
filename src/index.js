73560f7760e09702af493b564e0fb7ef6c43460f
Mike Jablonski <mjablonski@gmail.com>
2017-07-29 16:33:18 -0700

    added simicolon.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import ScrollToTop from './ScrollToTop'
+import ScrollToTop from './ScrollToTop';
 
 import { BrowserRouter } from 'react-router-dom';
 
