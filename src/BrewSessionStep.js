1f6c7c976be00b3ab45e7b491619e45c39f9ae67
Mike Jablonski <mjablonski@gmail.com>
2017-09-04 08:39:01 -0700

    updated status to include target temp hit flag.

--- src/BrewSessionStep.js
+++ src/BrewSessionStep.js
@@ -5,28 +5,33 @@ class BrewSessionStep extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {description: ''};
+    this.state = {description: '', step: 0, hasHitMashStepTemp: false, hasHitBoilStepTemp: false};
   }
 
   componentWillReceiveProps(nextProps) {
+      this.setState({
+          hasHitMashStepTemp: nextProps.brewSession.hasHitMashStepTemp,
+          hasHitBoilStepTemp: nextProps.brewSession.hasHitBoilStepTemp
+        });
+
       switch (nextProps.brewSession.step) {
           case 1:
-            this.setState({description: 'Heat Strike Water (1/5)'});
+            this.setState({description: 'Heat Strike Water (1/5)', step: 1});
             break;
         case 2:
-            this.setState({description: 'Transfer Water to MT (2/5)'});
+            this.setState({description: 'Transfer Water to MT (2/5)', step: 2});
             break;
         case 3:
-            this.setState({description: 'Mash (3/5)'});
+            this.setState({description: 'Mash (3/5)', step: 3});
             break;
         case 4:
-            this.setState({description: 'Transfer to BK (4/5)'});
+            this.setState({description: 'Transfer to BK (4/5)', step: 4});
             break;
         case 5:
-            this.setState({description: 'Boil (5/5)'});
+            this.setState({description: 'Boil (5/5)', step: 5});
             break;
         case 6:
-            this.setState({description: 'Done'});
+            this.setState({description: 'Done', step: 6});
             break;
         default:
             break;
@@ -35,7 +40,15 @@ class BrewSessionStep extends Component {
 
   render() {
     return (
-      <p>Step: {this.state.description}</p>
+        <div>
+            <p>Step: {this.state.description}</p>
+            { (this.state.step === 3) && 
+                <p>Hit Mash Temp: {this.state.hasHitMashStepTemp.toString()}</p>
+            }
+            { (this.state.step === 5) && 
+                <p>Hit Boil Temp: {this.state.hasHitBoilStepTemp.toString()}</p>
+            }
+        </div>
     );
   }
 }
