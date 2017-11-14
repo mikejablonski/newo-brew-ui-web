528fd021109b838681fb40fd07237a8d15af49d8
Mike Jablonski <mjablonski@gmail.com>
2017-11-14 07:39:27 -0800

    moved hhmmss format function to new file.

--- src/MashStepStatus.js
+++ src/MashStepStatus.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {toHHMMSS} from './lib/DateTimeFormatting';
 
 class MashStepStatus extends Component {
 
@@ -17,7 +18,9 @@ class MashStepStatus extends Component {
       return "N/A";
     }
     
-    var result = (this.props.brewSession.currentStepMinutesRemaining * 60).toString().toHHMMSS();
+    var secondsRemaining = this.props.brewSession.currentStepMinutesRemaining * 60;
+
+    var result = toHHMMSS(secondsRemaining.toString());
     return result;
   }  
 
@@ -37,16 +40,16 @@ class MashStepStatus extends Component {
   }
 }
 
-String.prototype.toHHMMSS = function () {
-  var sec_num = parseInt(this, 10); // don't forget the second param
-  var hours   = Math.floor(sec_num / 3600);
-  var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-  var seconds = sec_num - (hours * 3600) - (minutes * 60);
+// String.prototype.toHHMMSS = function () {
+//   var sec_num = parseInt(this, 10); // don't forget the second param
+//   var hours   = Math.floor(sec_num / 3600);
+//   var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+//   var seconds = sec_num - (hours * 3600) - (minutes * 60);
 
-  if (hours   < 10) {hours   = "0"+hours;}
-  if (minutes < 10) {minutes = "0"+minutes;}
-  if (seconds < 10) {seconds = "0"+seconds;}
-  return hours+':'+minutes+':'+seconds;
-}
+//   if (hours   < 10) {hours   = "0"+hours;}
+//   if (minutes < 10) {minutes = "0"+minutes;}
+//   if (seconds < 10) {seconds = "0"+seconds;}
+//   return hours+':'+minutes+':'+seconds;
+// }
 
 export default MashStepStatus;
